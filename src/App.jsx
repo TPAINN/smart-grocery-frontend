0b6df8d379cf6fb87efa19a0d7d70324872ace0a
Απόστολος Πεϊνίρης <apostolos.peiniris@gmail.com>
2026-03-05 11:08:35 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1560,7 +1560,7 @@ export default function App() {
           <div className="header-top">
             <div className="datetime-display">
               <div className="current-date">{timeGreeting} {timeIcon}</div>
-              <div className="current-time">{currentTime.toLocaleDateString('el-GR', { weekday:'short', day:'numeric', month:'long' })}</div>
+              <div className="current-time">{currentTime.toLocaleDateString('el-GR', { weekday:'long', day:'numeric', month:'long' })}</div>
               <div className="current-clock">{currentTime.toLocaleTimeString('el-GR', { timeZone:'Europe/Athens', hour:'2-digit', minute:'2-digit', second:'2-digit' })}</div>
             </div>
 
